3def9a431804c1fcb39f7a992fa041a195a35804
Keshab Kataruka <83745415+Keshab0100@users.noreply.github.com>
2022-05-23 14:44:53 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -59,33 +59,33 @@ app.post("/text_file_to_audio", (req, res) => {
 })
 
 // 4. Merge Image and Audio, notDONE
-app.post("/merge_image_and_audio", (req, res) => {
-    const images = ["../public/images/IMG01.jpg", "../public/images/IMG02.jpg", "../public/images/IMG03.jpg"]
-    var videoOptions = {
-        fps: 25,
-        loop: 5,
-        transition: true,
-        transitionDuration: 1,
-        format: 'mp4',
-    }
-    videoShow(images, videoOptions)
-        .audio('../public/Uploads/audio.mp3')
-        .save('../public/Uploads/video.mp4')
-        .on('start', function (command) {
-            console.log('ffmpeg process started:'+ command)
-        })
-        .on('error', function (err, stdout, stderr) {
-            console.log("Some error occured" + err)
-        })
-        .on('end', function (output) {
-            console.log('Conversion completed' + output)
-        })
-    res.send({
-        "status": "ok",
-        "message": "Video Created Successfully",
-        "video_file_path":"public/Uploads"
-    })
-})
+// app.post("/merge_image_and_audio", (req, res) => {
+//     const images = ["../public/images/IMG01.jpg", "../public/images/IMG02.jpg", "../public/images/IMG03.jpg"]
+//     var videoOptions = {
+//         fps: 25,
+//         loop: 5,
+//         transition: true,
+//         transitionDuration: 1,
+//         format: 'mp4',
+//     }
+//     videoShow(images, videoOptions)
+//         .audio('../public/Uploads/audio.mp3')
+//         .save('../public/Uploads/video.mp4')
+//         .on('start', function (command) {
+//             console.log('ffmpeg process started:'+ command)
+//         })
+//         .on('error', function (err, stdout, stderr) {
+//             console.log("Some error occured" + err)
+//         })
+//         .on('end', function (output) {
+//             console.log('Conversion completed' + output)
+//         })
+//     res.send({
+//         "status": "ok",
+//         "message": "Video Created Successfully",
+//         "video_file_path":"public/Uploads"
+//     })
+// })
 
 
 app.listen(port, () => {
